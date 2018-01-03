b3243daf7bde0a7acafdaf5e941f769ecad42eae
Alex Seif <alex.seif@gmail.com>
2018-01-03 17:42:17 +0200

    centering content

--- summury_1.html.php
+++ summury_1.html.php
@@ -2,46 +2,62 @@
 <!-- Page Content -->
 <div class="container">
   <h1 class="text-center"><img src='images/Sofitel Hotel Logo 150.png'/></h1>
-  <h2 class="text-center">Today's Events</h2>
-    <table class="table table-condensed table-responsive">
-      <tbody>
-        <?php
-        if (is_array($currentReservations)) {
-          foreach ($currentReservations as $reservation) {
-            ?>
-            <tr>
-              <td class="col-md-1 tbl-logo-bg"> 
-                <?php if (!in_array($reservation['arrowDirection'], array('right', 'down'))) { ?>
-                  <span class="glyphicon glyphicon-circle-arrow-<?php echo $reservation['arrowDirection']; ?>"></span>
-                <?php } ?>
-              </td>
-              <td class="col-md-4 tbl-title-bg">
-                <strong><?php echo $reservation['title']; ?></strong>
-              </td>
-              <td class="col-md-1 tbl-logo-bg">
-                <?php if (file_exists('./images/logos/' . $reservation['title'] . '.png')) { ?>
-                  <img class='text-center' src='images/logos/<?php echo $reservation['title'] ?>.png'/>
-                  <?php
-                }
-                ?>
-              </td>
-              <td class="col-md-4 tbl-title-bg">
-                <div style="border-bottom:2px solid #3fa63b;height: 50%"><?php echo $reservation['resourceName']; ?></div>
-                <div style="height: 50%"><?php echo $reservation['description'] ?></div>
-              </td>
-              <td class="col-md-1 tbl-logo-bg">
-                <?php if (in_array($reservation['arrowDirection'], array('right', 'down'))) { ?>
-                  <span class="glyphicon glyphicon-circle-arrow-<?php echo $reservation['arrowDirection']; ?>"></span>
-                <?php } ?>
-              </td>
-
-            </tr>
+  <?php
+  if (is_array($currentReservations)) {
+    $height = floor(65 / count($currentReservations));
+    ?>
+    <style>
+      .table > tbody > tr > td {
+        height: <?php echo $height; ?>vh;
+      }
+    </style>
+    <h2 class="text-center">Today's Events</h2>
+    <div class="table-holder">
+      <div class="table-div">
+        <table class="table">
+          <tbody>
             <?php
-          }
-        }
-        ?>
-      </tbody>
-    </table>
+            foreach ($currentReservations as $reservation) {
+              ?>
+              <tr>
+                <td class="col-md-1 tbl-logo-bg"> 
+                  <?php if (!in_array($reservation['arrowDirection'], array('right', 'down'))) { ?>
+                    <span class="glyphicon glyphicon-circle-arrow-<?php echo $reservation['arrowDirection']; ?>"></span>
+                  <?php } ?>
+                </td>
+                <td class="col-md-4 tbl-title-bg">
+                  <strong><?php echo $reservation['title']; ?></strong>
+                </td>
+                <td class="col-md-1 tbl-logo-bg">
+                  <?php if (file_exists('./images/logos/' . $reservation['title'] . '.png')) { ?>
+                    <img class='text-center' src='images/logos/<?php echo $reservation['title'] ?>.png'/>
+                    <?php
+                  }
+                  ?>
+                </td>
+                <td class="col-md-4 tbl-title-bg">
+                  <div style="border-bottom:2px solid #3fa63b;height: 50%"><?php echo $reservation['resourceName']; ?></div>
+                  <div style="height: 50%"><?php echo $reservation['description'] ?></div>
+                </td>
+                <td class="col-md-1 tbl-logo-bg">
+                  <?php if (in_array($reservation['arrowDirection'], array('right', 'down'))) { ?>
+                    <span class="glyphicon glyphicon-circle-arrow-<?php echo $reservation['arrowDirection']; ?>"></span>
+                  <?php } ?>
+                </td>
+
+              </tr>
+            <?php }
+            ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <?php
+  } else {
+    include_once 'room_ad.html.php';
+  }
+  ?>
+
 </div>
 <!-- /.container-fluid -->
 <?php include_once 'template/footer.html.php'; ?>
--- zone_1.html.php
+++ zone_1.html.php
@@ -2,47 +2,70 @@
 <!-- Page Content -->
 <div class="container">
   <h1 class="title text-center"><img src='images/Sofitel Hotel Logo 150.png'></h1>
-  <h2 class="text-center">Today's Event - <?php echo $floor_title; ?></h2>
-  <table class="table">
-    <tbody>
-      <?php
-      if (is_array($currentReservations)) {
-        foreach ($currentReservations as $reservation) {
-          ?>
-          <tr> 
-            <td class="col-md-1 tbl-logo-bg"> 
-              <?php if ("left" == $reservation['arrowDirection']) { ?>
-                <span class="glyphicon glyphicon-circle-arrow-left"></span>
-              <?php } ?>
-            </td>
-            <td class="col-md-4 tbl-title-bg">
-              <?php echo $reservation['title']; ?>
-            </td>
-            <td class="col-md-1 tbl-logo-bg">
-              <?php
-              if (file_exists('./images/logos/' . $reservation['title'] . '.png')) {
-                ?>
-                <img class='text-center' src='images/logos/<?php echo $reservation['title'] . '.png'; ?>' />
-                <?php
-              }
-              ?>
-            </td>
-            <td class="col-md-4 tbl-title-bg">
-              <div style="border-bottom:2px solid #3fa63b;height: 50%"><?php echo $reservation['description']; ?></div>
-              <div style="height: 50%"><?php echo $reservation['resourceName']; ?></div>
-            </td>
-            <td class="col-md-1 tbl-logo-bg"> 
-              <?php if ("right" == $reservation['arrowDirection']) { ?>
-                <span class="glyphicon glyphicon-circle-arrow-right"></span>
-              <?php } ?>
-            </td>
-          </tr>
-          <?php
-        }
+
+  <?php
+  if (is_array($currentReservations)) {
+    $height = floor(65 / count($currentReservations));
+    ?>
+    <style>
+      .table > tbody > tr > td {
+        height: <?php echo $height; ?>vh;
       }
-      ?>
-    </tbody>
-  </table>
+    </style>
+    <h2 class="text-center">Today's Event - <?php echo $floor_title; ?></h2>
+    <div class="table-holder">
+      <div class="table-div">
+        <table class="table">
+          <tbody>
+            <?php
+            foreach ($currentReservations as $reservation) {
+              ?>
+              <tr> 
+                <td class="col-md-1 tbl-logo-bg"> 
+                  <?php if ("left" == $reservation['arrowDirection']) { ?>
+                    <span class="glyphicon glyphicon-circle-arrow-left"></span>
+                  <?php } ?>
+                </td>
+                <td class="col-md-4 tbl-title-bg">
+                  <?php echo $reservation['title']; ?>
+                </td>
+                <td class="col-md-1 tbl-logo-bg">
+                  <?php
+                  if (file_exists('./images/logos/' . $reservation['title'] . '.png')) {
+                    ?>
+                    <img class='text-center' src='images/logos/<?php echo $reservation['title'] . '.png'; ?>' />
+                    <?php
+                  }
+                  ?>
+                </td>
+                <td class="col-md-4 tbl-title-bg">
+                  <div style="border-bottom:2px solid #3fa63b;height: 50%"><?php echo $reservation['description']; ?></div>
+                  <div style="height: 50%"><?php echo $reservation['resourceName']; ?></div>
+                </td>
+                <td class="col-md-1 tbl-logo-bg"> 
+                  <?php if ("right" == $reservation['arrowDirection']) { ?>
+                    <span class="glyphicon glyphicon-circle-arrow-right"></span>
+                  <?php } ?>
+                </td>
+              </tr>
+              <?php
+            }
+            ?>
+          </tbody>
+        </table>
+        <style>
+          .table > tbody > tr > td {
+            height: <?php echo $height; ?>vh;
+          }
+        </style>
+      </div>
+    </div>
+    <?php
+  } else {
+    include_once 'room_ad.html.php';
+  }
+  ?>
+
 </div>
 
 <!-- /.container -->
