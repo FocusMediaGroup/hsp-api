6fb64960dffd0478a7aee835924ba08f1f6442a8
Alex Seif <alex.seif@gmail.com>
2018-03-19 22:22:46 +0200

    Reser

--- template/head.html.php
+++ template/head.html.php
@@ -7,6 +7,7 @@
     <meta name="viewport" 
           content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta name="description" content="">
+    <meta http-equiv="cache-control" content="max-age=0" />
     <meta name="author" content="">
     <title><?php echo $title; ?></title>
     <!-- Bootstrap Core CSS -->
