dfb6fa5dedeb439867ccf931304602e3e737ef68
Alex Seif <alex.seif@gmail.com>
2019-11-13 11:20:09 +0200

    nav edits

--- admin/template/parts/sidbar.html.php
+++ admin/template/parts/sidbar.html.php
@@ -4,8 +4,8 @@
   <div class="sidebar-header d-flex align-items-center">
     <div class="avatar"><img src="assets/img/avatar-1.jpg" alt="..." class="img-fluid rounded-circle"></div>
     <div class="title">
-      <h1 class="h4">Mark Stephen</h1>
-      <p>Web Designer</p>
+      <h1 class="h4">HSP API</h1>
+      <p>By FMG</p>
     </div>
   </div>
   <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
